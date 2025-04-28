e0207786c3eb2768c708162b59c0ff0b11dee8fc
Gorchon <a01254831@tec.mx>
2025-04-28 13:07:59 -0600

    Test on dummy db running

--- src/main/java/com/example/demo/DBTests.java
+++ src/main/java/com/example/demo/DBTests.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSightTestContainersDumbApplication {
+public class DBTests {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSightTestContainersDumbApplication.class, args);
+		SpringApplication.run(DBTests.class, args);
 	}
 
 }

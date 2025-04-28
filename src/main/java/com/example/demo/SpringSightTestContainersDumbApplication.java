3c396f4607dbe4661190618a0b1f80d9b103fb3e
Gorchon <a01254831@tec.mx>
2025-04-28 13:04:23 -0600

    Test on dummy db running

--- src/main/java/com/example/demo/SpringSightTestContainersDumbApplication.java
+++ src/main/java/com/example/demo/SpringSightTestContainersDumbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DBTests {
+public class SpringSightTestContainersDumbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DBTests.class, args);
+		SpringApplication.run(SpringSightTestContainersDumbApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DBTestsApplicationTests.java
+++ src/test/java/com/example/demo/DBTestsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSightTestContainersDumbApplicationTests {
+class DBTestsApplicationTests {
 
 	@Test
 	void contextLoads() {
